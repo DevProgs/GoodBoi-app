0349be75ab4e1c7fb5cbd009c684fd840911e758
DevProgs <DevProgs1@gmail.com>
2023-03-22 03:53:31 -0500

    Rendering dog images through javascript

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
-import dogs from "/data.js"
+import dogData from "/data.js"
 import { likeBtn, dislikeBtn } from './buttons'
 
+class Dog {
+  constructor(data){
+    Object.assign(this, data)
+  }
+  getDogHtml(){
+    const {name, avatar, age, bio, hasBeenSwiped, hasBeenLiked} = this
+    return `
+      <img class="dog__img" src="/${avatar}" alt="${name}">
+      <h3 class="dog__info">${name}, ${age}</h3>
+      <p class="dog__intro">${bio}</p>
+    `;
+  }
+}
 
+const doggo = new Dog(dogData[0])
+
+const renderDog = () => {
+  document.getElementById('dog').innerHTML = doggo.getDogHtml()
+}
+
+renderDog()
 
 document.querySelector(".nope-btn").addEventListener('click', dislikeBtn)
 
