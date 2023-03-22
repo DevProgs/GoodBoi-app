3ff1e73223fdde3015179bac8bdea24606f6ff8f
DevProgs <DevProgs1@gmail.com>
2023-03-22 03:34:52 -0500

    Testing btn logic in seperate JS file

---
+++ buttons.js
@@ -0,0 +1,9 @@
+const likeBtn = () => {
+  console.log('like')
+}
+
+const dislikeBtn = () => {
+  console.log('dislike')
+}
+
+export {likeBtn, dislikeBtn}
--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 import dogs from "/data.js"
+import { likeBtn, dislikeBtn } from './buttons'
 
-console.log(dogs)
+
+
+document.querySelector(".nope-btn").addEventListener('click', dislikeBtn)
+
+document.querySelector(".like-btn").addEventListener('click', likeBtn)
